1d8c594baa424eb56ab9531f3361cbe629913d22
AdrianoRaffaulte <raffaulteadrianog@gmail.com>
2024-06-17 21:43:26 -0300

    Primer Commit

---
+++ TP1-Raffaulte.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EJERCICIO 1 - PHP - Raffaulte</title>
+</head>
+<body>
+    <h1>POO</h1>
+<?php
+    class Empleado {
+        public $nombre;
+        public $sueldo;
+
+        public function __construct($nombre, $sueldo) {
+            $this->nombre = $nombre;
+            $this->sueldo = $sueldo;
+        }
+
+        public function Nombre() {
+            return $this->nombre;
+        }
+
+        public function Sueldo() {
+            return $this->sueldo;
+        }
+
+        public function calcular_Salario() {
+            $salarioNeto = $this->sueldo * 0.9;
+            return $salarioNeto;
+        }
+    }
+
+    class Desarrollador extends Empleado {
+        public function calcular_Salario() {
+            return calcular_Salario();
+        }
+    }
+
+    class Diseñador extends Empleado {
+        public function calcular_Salario() {
+            $salarioNeto = $this->Sueldo() * 0.85;
+            return $salarioNeto;
+        }
+    }
+
+    $empleado1 = new Desarrollador("Adriano", 1500);
+    $empleado2 = new Diseñador("Valentin", 1500);
+
+    echo "Empleado 1 - Nombre: " . $empleado1->Nombre() . ", Salario Neto: $" . $empleado1->calcular_Salario();
+    echo "Empleado 2 - Nombre: " . $empleado2->Nombre() . ", Salario Neto: $" . $empleado2->calcular_Salario();
+
+?>
+
+</body>
+</html>
